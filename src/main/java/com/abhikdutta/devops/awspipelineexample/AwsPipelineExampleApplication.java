542d1ad402f3c48ac171346950b7aa97f5858e53
Abhik Dutta <abhik.it2011@gmail.com>
2019-11-17 16:04:31 +0530

    My First SpringBoot Project Pipeline for CI CD Pipeline in AWS

---
+++ src/main/java/com/abhikdutta/devops/awspipelineexample/AwsPipelineExampleApplication.java
@@ -0,0 +1,13 @@
+package com.abhikdutta.devops.awspipelineexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsPipelineExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsPipelineExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abhikdutta/devops/awspipelineexample/AwsPipelineExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhikdutta.devops.awspipelineexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsPipelineExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
